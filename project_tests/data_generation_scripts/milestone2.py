83b2a270a82d627bc81970148292cbb2ba21e6b0
Brian Hentschel <bhentschel@dhcp-10-250-80-93.harvard.edu>
2019-09-03 17:28:15 -0400

    first fix

--- project_tests/data_generation_scripts/milestone2.py
+++ project_tests/data_generation_scripts/milestone2.py
@@ -30,7 +30,7 @@ DOCKER_TEST_BASE_DIR = "/cs165/staff_test"
 
 def generateDataMilestone2(dataSize):
     outputFile = TEST_BASE_DIR + '/data3_batch.csv'
-    header_line = data_gen_utils.generateHeaderLine('db1', 'tbl3', 4)
+    header_line = data_gen_utils.generateHeaderLine('db1', 'tbl3_batch', 4)
     outputTable = pd.DataFrame(np.random.randint(0, dataSize/5, size=(dataSize, 4)), columns =['col1', 'col2', 'col3', 'col4'])
     # This is going to have many, many duplicates for large tables!!!!
     outputTable['col1'] = np.random.randint(0,1000, size = (dataSize))

82d73e57ee7a1ecb3089c1b827aee88fefe60293
Wilson Qin <wilson.qin@gmail.com>
2019-09-13 01:58:22 -0400

    add bash scripts to support milestone testing and case testing

---
+++ infra_scripts/test_milestone.sh
@@ -0,0 +1,30 @@
+UPTOMILE="${1:-5}"
+
+MAX_AVAILABLE_MS=5
+MAX_TEST=43
+TEST_IDS=`seq -w 1 ${MAX_TEST}`
+
+if [ "$UPTOMILE" -eq "1" ] ;
+then
+    MAX_TEST=9
+elif [ "$UPTOMILE" -eq "2" ] ;
+then
+    MAX_TEST=19
+elif [ "$UPTOMILE" -eq "3" ] ;
+then
+    MAX_TEST=29
+elif [ "$UPTOMILE" -eq "4" ] ;
+then
+    MAX_TEST=39
+elif [ "$UPTOMILE" -eq "5" ] ;
+then
+    MAX_TEST=43
+fi
+
+for TEST_ID in $TEST_IDS
+do
+    if [ "$TEST_ID" -le "$MAX_TEST" ]
+    then
+        make run_test test_id=$TEST_ID
+    fi
+done
